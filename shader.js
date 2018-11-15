ed9ca84e458f5be448a7ae14a329e5f57944e243
Lutz Reiter <lutz.reiter@vw-fc.com>
2018-11-15 12:35:17 +0100

    Put shader in different file

--- shader.js
+++ shader.js
@@ -1,6 +1,5 @@
 const canvasSketch = require('canvas-sketch');
 const createShader = require('canvas-sketch-util/shader');
-const glsl = require('glslify');
 
 // Setup our sketch
 const settings = {
@@ -10,17 +9,7 @@ const settings = {
 
 // Your glsl code
 // for code hoghliting install shader language support and comment tagged templates
-const frag = glsl(/* glsl */`
-  precision highp float;
-
-  uniform float time;
-  varying vec2 vUv;
-
-  void main () {
-    vec3 color = 0.5 + 0.5 * cos(time + vUv.xyx + vec3(0.0, 2.0, 4.0));
-    gl_FragColor = vec4(color, 1.0);
-  }
-`);
+const frag = require('./circle.frag');
 
 // Your sketch, which simply returns the shader
 const sketch = ({ gl }) => {
